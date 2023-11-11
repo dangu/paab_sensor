317c8d1e9a5d4e066120ded7e364a3c67ee3a2ac
Daniel Gullberg <daniel_gullberg@hotmail.com>
2023-11-11 17:57:26 +0100

    Use value/1000.0 and only send when changed

--- transceiver.py
+++ transceiver.py
@@ -11,16 +11,19 @@ class Transceiver:
     """Transmitter/receiver class"""
     def __init__(self, filterTime, location):
         """Init"""
-        self._dataDictFilt = {'water_level': []}
+        self._dataDictFilt = {'waterLevel': []}
+        self._lastValueDict = {'waterLevel':None}
         self._filterTimeSecs = filterTime
         self._location = location
         self._t0 = 0
+        self._onlySendWhenChanged = True
 
         self._logger = logging.getLogger("Transceiver")
         
     def _filterAndSendData(self,name,value):
         """Get data from sensor and send over TCP/IP"""
         dataDict = {}
+        decimals=3
         self._dataDictFilt[name].append(value)
 
         tNow = time.time()
@@ -28,17 +31,22 @@ class Transceiver:
             self._logger.debug("Filter time reached")
             self._t0 = tNow
             sampleDict = {}
+            valueChanged = False
             for key, valueList in self._dataDictFilt.items():
-                valFilt = round(statistics.median(valueList),2)
+                valFilt = round(statistics.median(valueList),3)
                 sampleDict[key] = valFilt
+                if self._lastValueDict[key] != valFilt:
+                    valueChanged = True
+                self._lastValueDict[key] = valFilt
                 self._dataDictFilt[key] = []
                 self._logger.info(f"{key}: {valueList} => {valFilt})")
-                
-            packetDict ={'name': 'water_level',
-                         'tags': {'location':self._location},
-                         'dataDict': sampleDict}
-            self._publisher.send(packetDict)
 
+            if not self._onlySendWhenChanged or valueChanged:
+                packetDict ={'name': 'waterLevel',
+                             'tags': {'location':self._location},
+                             'dataDict': sampleDict}
+                self._publisher.send(packetDict)
+                self._logger.info("Send changed data")
         
     def connectAndRun(self,
                       port,
@@ -57,9 +65,10 @@ class Transceiver:
 
         while(True):
             try:
-                waterLevel=paabSensor.getReg1Value()
-
-                self._filterAndSendData(name="water_level",
+                waterLevelInt=paabSensor.getReg1Value()
+                # Use water level in meters
+                waterLevel=waterLevelInt/1000.0
+                self._filterAndSendData(name="waterLevel",
                                         value=waterLevel)
                 time.sleep(sampleTime)
             except ValueError as e:
